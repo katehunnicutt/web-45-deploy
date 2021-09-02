59b9c4409e5d1f091dfe53756a7a04d468d3c36f
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-09-02 09:58:12 -0700

    deployable

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const server = require("express")()
+const express = require("express")
+const cors = require("cors")
 
-server.listen(5000, () => {
-  console.log("listening on port 5000")
+console.log(process.env.TERM)
+console.log(process.env.PORT)
+
+const PORT = process.env.PORT || 5000
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.get("/", (req, res) => {
+  res.send("<h1> oms oms </h1>")
+})
+
+server.get("/api", (req, res) => {
+  res.json({message: "gabe says he likes us"})
+})
+
+server.listen(process.env.PORT, () => {
+  console.log(`listening on port ${PORT}`)
 })
